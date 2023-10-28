94a13b1922e6b70529f508ee3dc8e1729ba8f92e
Yyax13 <Yyax13@proton.me>
2023-10-28 10:19:49 -0300

    Created and finished hkolds.py

--- hkolds.py
+++ hkolds.py
@@ -1,2 +1,12 @@
 from misc import update
 update.pip(package="yollor")
+update.pip(package="requests")
+import requests
+import json
+
+ReqData = requests.get("https://hkolds.vercel.app/api/v1/version")
+
+data = json.loads(ReqData.text)
+version = data['projectVersion']
+state = data['status']
+print(f"O Projeto está na versão {version} e com status em {state}")
