bcd1cbbb3f9f454234475995bca5c970761789dd
Yyax13 <Yyax13@proton.me>
2023-10-30 16:43:55 +0000

    Finish the clima.py

--- clima.py
+++ clima.py
@@ -7,7 +7,7 @@ token = "debf6ffe6409427fea9a0cf27020e78f"
 ReqData = r.get(f"http://apiadvisor.climatempo.com.br/api/v1/anl/synoptic/locale/BR?token={token}")
 data = json.loads(ReqData.text)
 
-country = data['country']
-text = data['text']
-date = data['date']
+country = data[0]['country']
+text = data[0]['text']
+date = data[0]['date']
 print(f"No país {country} no dia {date}, as informações sobre o clima são: {text}")
